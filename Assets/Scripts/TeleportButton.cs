b3ad31e759256a05a91954df5f8891d3b49312e5
Colloidal <389240875@qq.com>
2019-10-01 20:55:29 +0800

    button传送，地板自由teleport

---
+++ Assets/Scripts/TeleportButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class TeleportButton : MonoBehaviour
+{
+    public HoverButton hoverButton;
+
+    public GameObject player;
+
+    public Transform teleportPos;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        hoverButton.onButtonDown.AddListener(OnButtonDown);
+        
+    }
+
+    private void OnButtonDown(Hand hand)
+    {
+        StartCoroutine(DoTeleport());
+    }
+
+    public void TeleportToNextPlatform()
+    {
+        Vector3 pos = teleportPos.position;
+        player.transform.position = pos;
+    }
+
+    private IEnumerator DoTeleport()
+    {
+        player.transform.position = teleportPos.position;
+        yield return null;
+    }
+}
