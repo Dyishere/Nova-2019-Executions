f2a989300e5d9aeb4fc3c996080b608773ebfd8f
xindu233 <32609399+xindu233@users.noreply.github.com>
2019-10-07 15:31:27 +0800

    “s”

--- Assets/Scripts/DamageSystem/TestDamage.cs
+++ Assets/Scripts/DamageSystem/TestDamage.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class TestDamage : MonoBehaviour
 {
+    public Text test;
     private DamageSystem m_damageSystem;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +18,15 @@ public class TestDamage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SteamVR_Actions.buggy_Brake.stateDown == true)
+        {
+            test.text = "buggy_Brake";
+        }
+        if (SteamVR_Actions.buggy_Reset.stateDown == true)
+        {
+            test.text = "p_buggy_Reset";
+        }
+
     }
 
     private void OnMouseEnter()
@@ -32,4 +44,6 @@ public class TestDamage : MonoBehaviour
         m_damageSystem.Damage(20);
         Debug.Log(name + "血量为" + m_damageSystem.GetCurHealth() + "，状态为" + m_damageSystem.GetCurState());
     }
+
+
 }
