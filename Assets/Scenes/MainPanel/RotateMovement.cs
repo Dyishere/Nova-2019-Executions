7aecef40baa7537337776fb7f222e1f7d40a4a27
xindu233 <32609399+xindu233@users.noreply.github.com>
2019-10-11 23:22:35 +0800

    添加ui

---
+++ Assets/Scenes/MainPanel/RotateMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMovement : MonoBehaviour
+{
+    float MAXROTATE = 300f;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.eulerAngles += new Vector3(0, MAXROTATE * Time.deltaTime, 0);
+    }
+}
