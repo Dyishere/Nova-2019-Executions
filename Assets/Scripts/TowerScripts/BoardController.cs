b8d2c79dfc24f98a5fddc2e1060f50e54bd2579e
Colloidal <389240875@qq.com>
2019-10-13 19:08:57 +0800

    塔技能调整

--- Assets/Scripts/TowerScripts/BoardController.cs
+++ Assets/Scripts/TowerScripts/BoardController.cs
@@ -6,16 +6,11 @@ using UnityEngine.UI;
 public class BoardController : MonoBehaviour
 {
     public string curTower;
-    private Text protectFieldCDText;
-    private Text protectFieldEffectText;
-    private Text areaDamageCDText;
-    private Text speedUpCDText;
-    private Text speedUpEffectText;
+    private Text CDText;
+    private Text EffectText;
     private Text healthText;
-    private GameObject areaDamageTarget;
-    private GameObject protectionFieldTarget;
+    private GameObject skillTarget;
     private GameObject healthTarget;
-    private GameObject speedUpTarget;
 
     // Start is called before the first frame update
     private void Start()
@@ -32,30 +27,27 @@ public class BoardController : MonoBehaviour
     }
     public void CheckValue()
     {
-        protectFieldCDText.text = "充能所需时间：" + (protectionFieldTarget.GetComponent<ProtectionField>().GetCurValue("CDTime")) + "秒" ;
-        protectFieldEffectText.text = "剩余效果时间：" + (protectionFieldTarget.GetComponent<ProtectionField>().GetCurValue("EffectiveTime")) + "秒";
-        areaDamageCDText.text = "充能所需时间：" + (areaDamageTarget.GetComponent<AreaDamageController>().GetCurValue("CDTime")) + "秒";
         healthText.text = "当前剩余血量：" + healthTarget.GetComponent<DamageSystem>().GetCurHealth();
-        if (curTower == "c")
+        switch (curTower)
         {
-            speedUpCDText.text = "充能所需时间：" + (speedUpTarget.GetComponent<ShootingSpeedUp>().GetCurValue("CDTime")) + "秒";
-            speedUpEffectText.text = "剩余效果时间：" + (speedUpTarget.GetComponent<ShootingSpeedUp>().GetCurValue("EffectiveTime")) + "秒";
+            case "a":
+                CDText.text = "充能所需时间：" + (skillTarget.GetComponent<AreaDamageController>().GetCurValue("CDTime")) + "秒";
+                break;
+            case "b":
+                CDText.text = "充能所需时间：" + (skillTarget.GetComponent<ProtectionField>().GetCurValue("CDTime")) + "秒";
+                break;
+            case "c":
+                CDText.text = "充能所需时间：" + (skillTarget.GetComponent<ShootingSpeedUp>().GetCurValue("CDTime")) + "秒";
+                break;
         }
+
+
     }
     private void InitTowerBoard()
     {
-        if (curTower == "c")
-        {
-            speedUpEffectText = GameObject.Find("c/Board/BoardCanvas/SpeedUpBoard/Effect").gameObject.GetComponent<Text>();
-            speedUpCDText = GameObject.Find("c/Board/BoardCanvas/SpeedUpBoard/CD").gameObject.GetComponent<Text>();
-            speedUpTarget = GameObject.Find("platform/" + curTower + "/SpeedUpButton/SpeedUpPoint");
-        }
-        protectFieldEffectText = GameObject.Find(curTower + "/Board/BoardCanvas/ProtectionFieldBoard/Effect").gameObject.GetComponent<Text>();
-        protectFieldCDText = GameObject.Find(curTower + "/Board/BoardCanvas/ProtectionFieldBoard/CD").gameObject.GetComponent<Text>();
-        areaDamageCDText = GameObject.Find(curTower + "/Board/BoardCanvas/AreaDamageBoard/CD").gameObject.GetComponent<Text>();
+        CDText = GameObject.Find(curTower + "/Board/BoardCanvas/SkillBoard/CD").gameObject.GetComponent<Text>();
         healthText = GameObject.Find(curTower + "/Board/BoardCanvas/HealthBoard/Health").gameObject.GetComponent<Text>();
-        areaDamageTarget = GameObject.Find("platform/" + curTower + "/AreaDamageButton/AreaDamagePoint");
-        protectionFieldTarget = GameObject.Find("platform/" + curTower + "/ProtectionFieldButton/ProtectionFieldPoint");
+        skillTarget = GameObject.Find(curTower + "/SkillButton/SkillPoint");
         healthTarget = GameObject.Find("platform/" + curTower);
     }
 }
